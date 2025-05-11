f701f1afa421dd8ffe4a282ad4bff38e689f812e
Elivicti <edwynb72@gmail.com>
2025-05-11 22:13:50 +0800

    update basic keymaps

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,13 +1,13 @@
 
-vim.keymap.set({ "n", "i" }, "<C-z>", "<Cmd>undo<CR>", { silent = true })
-vim.keymap.set({ "n", "i" }, "<C-r>", "<Cmd>redo<CR>", { silent = true })
+vim.keymap.set({ "n", "i", "v" }, "<C-z>", "<Cmd>undo<CR>", { silent = true })
+vim.keymap.set({ "n", "i", "v" }, "<C-r>", "<Cmd>redo<CR>", { silent = true })
 -- vim.keymap.set({ "n", "i" }, "<C-x>", "dd", { silent = true })
 
 
 vim.keymap.set("n", "j", "k", { remap = false }) -- swap up and down
 vim.keymap.set("n", "k", "j", { remap = false })
 
-vim.keymap.set({ "n", "i" }, "<C-s>", "<Cmd>write<CR>", { })
+vim.keymap.set({ "n", "i" }, "<C-s>", "<Cmd>write<CR><Esc>", { })
 
 -- open terminal mode, the tailing `i` is needed
 vim.keymap.set("n", "<C-t>", "<Cmd>terminal<CR>i", { silent = true })
