774fffdcbfab1941029c8103308a90c1a3a64384
Elivicti <edwynb72@gmail.com>
2025-05-13 18:11:39 +0800

    improve theme management

--- init.lua
+++ init.lua
@@ -3,3 +3,34 @@ require("core.keymap")
 require("core.lazy")   -- plugin manager
 
 
+local function set_bufferline_highlights()
+	vim.api.nvim_set_hl(0, "BufferLineDiagnosticError", { fg = "#db4b4b" })
+	vim.api.nvim_set_hl(0, "BufferLineDiagnosticWarn",  { fg = "#e0af68" })
+	vim.api.nvim_set_hl(0, "BufferLineDiagnosticInfo",  { fg = "#0db9d7" })
+	vim.api.nvim_set_hl(0, "BufferLineDiagnosticHint",  { fg = "#10B981" })
+end
+
+local color_schemes = {
+	tokyonight = {
+		name = "tokyonight",
+		config = function()
+			set_bufferline_highlights()
+		end
+	},
+	vscode = {
+		name = "vscode",
+		config = function()
+			set_bufferline_highlights()
+			vim.cmd("highlight RainbowDelimiterYellow guifg=#FFD700")
+			vim.cmd("highlight RainbowDelimiterViolet guifg=#DA70D6")
+			vim.cmd("highlight RainbowDelimiterBlue   guifg=#179FFF")
+		end
+	}
+}
+
+local function set_colorscheme(scheme)
+	vim.cmd(string.format("colorscheme %s", scheme.name))
+	scheme.config()
+end
+
+set_colorscheme(color_schemes.vscode)
--- lua/plugins/style-tokyonight.lua
+++ lua/plugins/style-tokyonight.lua
@@ -5,10 +5,5 @@ return {
 	},
 	config = function (_, opts)
 		require("tokyonight").setup(opts)
-		vim.cmd("colorscheme tokyonight")
-		vim.api.nvim_set_hl(0, "BufferLineDiagnosticError", { fg = "#db4b4b" })
-		vim.api.nvim_set_hl(0, "BufferLineDiagnosticWarn",  { fg = "#e0af68" })
-		vim.api.nvim_set_hl(0, "BufferLineDiagnosticInfo",  { fg = "#0db9d7" })
-		vim.api.nvim_set_hl(0, "BufferLineDiagnosticHint",  { fg = "#10B981" })
 	end
 }
