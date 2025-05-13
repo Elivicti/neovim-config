86b7a12131aaa5c8c1ebbcea90470c51b3e7811b
Elivicti <edwynb72@gmail.com>
2025-05-13 19:26:25 +0800

    swap Ctrl+Arrow and Shift+Arrow

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -10,6 +10,19 @@ vim.keymap.set({ "n", "i" }, "<C-s>", "<Cmd>write<CR><Esc>", { })
 vim.keymap.set("n", "<C-t>", "<Cmd>terminal<CR>i", { silent = true })
 
 
+local opts = { noremap = true, silent = true } -- 禁止递归映射且静默执行
+
+-- swap Ctrl+Arrow and Shift+Arrow
+vim.keymap.set({ "n", "v" }, "<S-Left>",  "<C-Left>",  opts)
+vim.keymap.set({ "n", "v" }, "<S-Right>", "<C-Right>", opts)
+vim.keymap.set({ "n", "v" }, "<S-Up>",    "<C-Up>",    opts)
+vim.keymap.set({ "n", "v" }, "<S-Down>",  "<C-Down>",  opts)
+--
+vim.keymap.set({ "n", "v" }, "<C-Left>",  "<S-Left>",  opts)
+vim.keymap.set({ "n", "v" }, "<C-Right>", "<S-Right>", opts)
+vim.keymap.set({ "n", "v" }, "<C-Up>",    "<S-Up>",    opts)
+vim.keymap.set({ "n", "v" }, "<C-Down>",  "<S-Down>",  opts)
+
 -- note: may be needed for lazy.nvim?
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
