e8ab62cf75ebb0f674b680a70c6792195382e0e5
Elivicti <edwynb72@gmail.com>
2025-05-13 20:35:37 +0800

    update keymaps

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -1,16 +1,17 @@
 
 vim.keymap.set({ "n", "i", "v" }, "<C-z>", "<Cmd>undo<CR>", { silent = true })
 vim.keymap.set({ "n", "i", "v" }, "<C-r>", "<Cmd>redo<CR>", { silent = true })
--- vim.keymap.set({ "n", "i" }, "<C-x>", "dd", { silent = true })
+vim.keymap.set("n", "<C-x>",      "dd",  { silent = true })
+vim.keymap.set("i", "<C-x>", "<Esc>ddi", { silent = true }) -- execute "dd" in insert mode
 
 
-vim.keymap.set({ "n", "i" }, "<C-s>", "<Cmd>write<CR><Esc>", { })
+vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<Cmd>write<CR><Esc>", { })
 
 -- open terminal mode, the tailing `i` is needed
 vim.keymap.set("n", "<C-t>", "<Cmd>terminal<CR>i", { silent = true })
 
 
-local opts = { noremap = true, silent = true } -- 禁止递归映射且静默执行
+local opts = { noremap = true, silent = true }
 
 -- swap Ctrl+Arrow and Shift+Arrow
 vim.keymap.set({ "n", "v" }, "<S-Left>",  "<C-Left>",  opts)
@@ -23,6 +24,6 @@ vim.keymap.set({ "n", "v" }, "<C-Right>", "<S-Right>", opts)
 vim.keymap.set({ "n", "v" }, "<C-Up>",    "<S-Up>",    opts)
 vim.keymap.set({ "n", "v" }, "<C-Down>",  "<S-Down>",  opts)
 
--- note: may be needed for lazy.nvim?
+-- needed for plugin keymaps
 vim.g.mapleader = " "
-vim.g.maplocalleader = "\\"
+vim.g.maplocalleader = ","
