313bdf23704174385010ab99d13bef5beb2ffc84
Elivicti <edwynb72@gmail.com>
2025-05-16 13:36:55 +0800

    update keymaps

--- lua/core/keymap.lua
+++ lua/core/keymap.lua
@@ -6,17 +6,22 @@ local silent  = { silent = true }
 local noremap = { silent = true, noremap = true }
 
 
-vim.keymap.set({ "n", "i", "v" }, "<C-z>", "<Cmd>undo<CR>", silent)
-vim.keymap.set({ "n", "i", "v" }, "<C-r>", "<Cmd>redo<CR>", silent)
-vim.keymap.set("n", "<C-x>",      "dd",  silent)
-vim.keymap.set("i", "<C-x>", "<Esc>ddi", silent) -- execute "dd" in insert mode
-
+vim.keymap.set({ "n", "i", "v" }, "<C-z>", "<Cmd>undo<CR>",     silent)
+vim.keymap.set({ "n", "i", "v" }, "<C-r>", "<Cmd>redo<CR>",     silent)
+vim.keymap.set({ "n", "i" },      "<C-x>", "<Cmd>norm dd<CR>",  silent)
 
 vim.keymap.set({ "n", "i", "v" }, "<C-s>", "<Cmd>write<CR><Esc>", { })
 
 -- open terminal mode, the tailing `i` is needed
 vim.keymap.set("n", "<A-t>", "<Cmd>terminal<CR>i", silent)
 
+-- Ctrl+c copys to system clipboard
+vim.keymap.set("n", "<C-c>", '"+yy', silent)
+vim.keymap.set("v", "<C-c>", '"+y',  silent)
+
+-- Ctrl+/ toggles comment line
+vim.keymap.set({ "n", "i" }, "<C-_>", "<Cmd>norm gcc<CR>", noremap)
+vim.keymap.set("v",          "<C-_>", "<Cmd>norm gc<CR>",  noremap)
 
 -- swap Ctrl+Arrow and Shift+Arrow
 vim.keymap.set({ "n", "v" }, "<S-Left>",  "<C-Left>",  noremap)
