5672ab31e00795a2c92ef955ec6f4c53f1d19941
Elivicti <edwynb72@gmail.com>
2026-01-10 15:12:30 +0800

    refactor colorcheme management

--- init.lua
+++ init.lua
@@ -2,29 +2,6 @@ require("core.basic")
 require("core.keymap")
 require("core.lazy")   -- plugin manager
 
-local color_schemes = {
-	tokyonight = {
-		name = "tokyonight",
-		config = function()
-		end
-	},
-	vscode = {
-		name = "vscode",
-		config = function()
-			vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#FFD700" })
-			vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#DA70D6" })
-			vim.api.nvim_set_hl(0, "RainbowDelimiterBlue",   { fg = "#179FFF" })
-		end
-	}
-}
-
-local function set_colorscheme(scheme)
-	vim.cmd("colorscheme " .. scheme.name)
-	scheme.config()
-end
-
-set_colorscheme(color_schemes.tokyonight)
-
 vim.api.nvim_create_autocmd("VimEnter", {
 	callback = function ()
 		vim.schedule(function ()
@@ -34,3 +11,5 @@ vim.api.nvim_create_autocmd("VimEnter", {
 		end)
 	end
 })
+
+vim.cmd.colorscheme("tokyonight")
--- lua/plugins/style-vscode.lua
+++ lua/plugins/style-vscode.lua
@@ -5,5 +5,16 @@ return {
 	},
 	config = function (_, opts)
 		require("vscode").setup(opts)
+
+		vim.api.nvim_create_autocmd("ColorScheme", {
+			pattern = "vscode",
+			callback = function()
+				assert(vim.g.colors_name == "vscode")
+
+				vim.api.nvim_set_hl(0, "RainbowDelimiterYellow", { fg = "#FFD700" })
+				vim.api.nvim_set_hl(0, "RainbowDelimiterViolet", { fg = "#DA70D6" })
+				vim.api.nvim_set_hl(0, "RainbowDelimiterBlue",   { fg = "#179FFF" })
+			end
+		})
 	end
 }
