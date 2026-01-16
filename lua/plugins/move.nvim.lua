8945d28cf5f7b7ddcd65bede2a3b4ef38d399edc
Elivicti <edwynb72@gmail.com>
2026-01-16 23:51:51 +0800

    fix move block keymap

--- lua/plugins/move.nvim.lua
+++ lua/plugins/move.nvim.lua
@@ -8,12 +8,12 @@ return {
 	},
 	keys = {
 		-- Normal Mode
-		{ "<A-Up>",    "<Cmd>MoveLine(-1)<CR>",   desc = "Move Line Up" },
-		{ "<A-Down>",  "<Cmd>MoveLine( 1)<CR>",   desc = "Move Line Down" },
+		{ "<A-Up>",    "<Cmd>MoveLine(-1)<CR>", desc = "Move Line Up" },
+		{ "<A-Down>",  "<Cmd>MoveLine( 1)<CR>", desc = "Move Line Down" },
 		-- Visual Mode
-		{ "<A-Up>",    "<Cmd>MoveBlock(-1)<CR>",  mode = { "v" }, desc = "Move Block Up" },
-		{ "<A-Down>",  "<Cmd>MoveBlock( 1)<CR>",  mode = { "v" }, desc = "Move Block Down" },
-		{ "<A-Left>",  "<Cmd>MoveHBlock(-1)<CR>", mode = { "v" }, desc = "Move Block Left" },
-		{ "<A-Right>", "<Cmd>MoveHBlock( 1)<CR>", mode = { "v" }, desc = "Move Block Right" },
+		{ "<A-Up>",    ":MoveBlock(-1)<CR>",  mode = { "v" }, desc = "Move Block Up" },
+		{ "<A-Down>",  ":MoveBlock( 1)<CR>",  mode = { "v" }, desc = "Move Block Down" },
+		{ "<A-Left>",  ":MoveHBlock(-1)<CR>", mode = { "v" }, desc = "Move Block Left" },
+		{ "<A-Right>", ":MoveHBlock( 1)<CR>", mode = { "v" }, desc = "Move Block Right" },
 	}
 }
